b2403df51d7654a7d1a0f2de589b51fbdea376da
EmekaG <nellychukwemeka@gmail.com>
2022-09-09 06:20:22 -0700

    5print c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
- */
+*main - Entry point
+*
+*Return: (By default returns zero if no errors otherwise return non-zero value)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
-
-            
-  
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0)
+}	  
