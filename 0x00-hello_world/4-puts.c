2de9c09c45c8822903e378fdb35baa1649802811
EmekaG <nellychukwemeka@gmail.com>
2022-09-10 03:20:22 -0700

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
-*main - Entry point
-*
-*Return: (By default returns zero if no errors otherwise return non-zero value)
-*/
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0)
-}	  
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
