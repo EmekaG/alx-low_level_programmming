ac57df9d833b7851ed746f5e0399f1041027890f
EmekaG <nellychukwemeka@gmail.com>
2022-09-13 12:43:44 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-	putchar('\n');
+		_putchar(letter);
+	_putchar('\n');
 
 	return (0);
 }
