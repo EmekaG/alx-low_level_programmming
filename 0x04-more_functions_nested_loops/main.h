113dde673132aed5167e1113e4b50b3846e583b7
EmekaG <nellychukwemeka@gmail.com>
2022-09-15 16:18:19 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * File - main.h
+ *
+ * Description - This is a header file with my contents for all the functions used in the 0x04-more_functions_nested_loops directory.
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+
+#endif
