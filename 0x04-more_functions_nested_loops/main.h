b23ac8d130df8cda613d0cb4aaa9b49bc6758ce8
EmekaG <nellychukwemeka@gmail.com>
2022-09-15 17:45:13 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@
  * Description - This is a header file with my contents for all the functions used in the 0x04-more_functions_nested_loops directory.
  */
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
